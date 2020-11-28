2de65a33817fa9bef95dbd739aa79af26e428e63
William Gabriel da Paz Rosendo <52809519+ma1kinho@users.noreply.github.com>
2020-11-27 21:35:31 -0300

    Add files via upload

---
+++ ProjectStatus.java
@@ -0,0 +1,16 @@
+package entities.enums;
+
+public enum ProjectStatus {
+	IN_ELABORATION, IN_PROGRESS, FINISHED;
+
+}
+
+/*private Integer num;
+
+ProjectStatus(int num) {
+	this.num = num;
+}
+
+public Integer getProjectStatus() {
+	return num;
+}*/
